dc7d09f3b703406f24ab3d2e9cd69d4f68296dc2
Alvaro Santos <siruz_slipknot@hotmail.com>
2023-05-09 07:31:53 -0300

    Projeto

---
+++ semana-transformacao-dev-cod3r/bitcent/src/components/landing/vantagens/Vantagem.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import ImagemResponsiva from "../comum/ImagemResponsiva"
+
+export interface VantagemProps {
+    imagem: any
+    titulo: string
+    subtitulo: string
+    inverter?: boolean
+}
+
+export default function Vantagem(props: VantagemProps) {
+    return (
+        <div className={`
+            flex flex-col justify-around items-center w-full gap-6
+            ${props.inverter ? 'sm:flex-row-reverse' : 'sm:flex-row'}
+        `}>
+            <ImagemResponsiva
+                imagem={props.imagem}
+                className={props.inverter ? 'sm:rotate-6' : 'sm:-rotate-6'}
+            />
+            <div className={`
+                flex flex-col gap-y-6 sm:w-[350px]
+                text-center sm:text-left
+            `}>
+                <div className={`
+                    flex flex-col text-white
+                    font-black text-2xl sm:text-4xl
+                `}>{props.titulo}</div>
+                <span className="font-light text-base sm:text-lg text-zinc-500">
+                    {props.subtitulo}
+                </span>                
+            </div>
+        </div>
+    )
+}
